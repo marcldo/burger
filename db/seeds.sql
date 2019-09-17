c368f332c8057dd3496e8b884132c1e0427ef389
gitMarcldo <marcldo@users.noreply.github.com>
2019-09-17 10:21:27 -0400

    installed npm packages and db schema & seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE `burgers_db`;
+
+INSERT INTO burgers (burger_name, devoured) VALUES('Cheese Burger',FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES('Double Cheese Burger',FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES('Chicken Burger',FALSE);
