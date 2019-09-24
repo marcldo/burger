711ee7a326937783345a7eac7a77434cb111852f
gitMarcldo <marcldo@users.noreply.github.com>
2019-09-24 19:30:55 -0400

    connection error fix

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = null;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createonnection(process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
         host: "localhost",
