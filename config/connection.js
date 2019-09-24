4c438c98d5cc257635eeb5c11db2f9db3ad064f6
gitMarcldo <marcldo@users.noreply.github.com>
2019-09-24 18:53:45 -0400

    jaws db connection variables

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createonnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
+
 
 // Make connection.
 connection.connect(function (err) {
